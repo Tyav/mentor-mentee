10478840c2b1b5e9d4d7306dab67f855c92f4f31
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-06 07:23:45 +0100

    added login method to auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const { celebrate: validate, errors } = require('celebrate');
-const User = require('../models/user.model')
+const User = require('../models/user.model');
 const userCtrl = require('../controllers/user.controller');
 const paramValidation = require('../validations/user.validation');
 
-const router = express.Router(); // eslint-disable-line new-cap 
+const router = express.Router(); // eslint-disable-line new-cap
 
-router.post ('/login', async (req,res) => {
-    const user = User.findOne({email : req.body.email})
-    console.log(user);
-    // if(!user) return  throw new Api
-    
+router.post('/login', async (req, res) => {
+  const user = User.findOne({ email: req.body.email });
   
-    // const user = await 
-})  
-  
-module.exports = router; 
+
+  // const user = await
+});
+
+module.exports = router;
