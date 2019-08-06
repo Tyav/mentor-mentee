dc662fe2fc23efee85248a41f69edda3dc68239f
MAC <mac@MACs-MacBook-Pro.local>
2019-08-06 14:16:40 +0100

    completed helper method for sending mails

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
-const httpStatus = require("http-status");
-const User = require("../models/user.model");
-const sendResponse = require("../helpers/response");
+const httpStatus = require('http-status');
+const User = require('../models/user.model');
+const sendResponse = require('../helpers/response');
+const nodemailer = require('nodemailer');
 
 /**
  * Load user and append to req.
@@ -8,18 +9,37 @@ const sendResponse = require("../helpers/response");
 exports.load = async (req, res, next, id) => {
   try {
     // Load user object from quarystring Id
-    return res.json(sendResponse(
-      httpStatus.NOT_FOUND,
-      "No such user exists!",
-      null,
-      null
-    ))
+    return res.json(
+      sendResponse(httpStatus.NOT_FOUND, 'No such user exists!', null, null)
+    );
   } catch (error) {
     next(error);
   }
-}; 
+};
 
 exports.getUsers = (req, res) => {
-  return res.json(sendResponse(200,'testing', null, null))
-}
+  return res.json(sendResponse(200, 'testing', null, null));
+};
 
+exports.sendMail = (email, message) => {
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    port: 25,
+    auth: {
+      user: process.env.MAILER_EMAIL,
+      pass: process.env.MAILER_PASSWORD
+    }
+  });
+  let mailOptions = {
+    to: email,
+    from: 'passwordreset@mentordev.com',
+    subject: 'Mentor Dev Password Reset',
+    text: message
+  };
+  smtpTransport.sendMail(mailOptions, err => {
+    if (err) {
+      return new Error(err);
+    }
+  });
+};
