2f26b9720119aa689364bfd1c85e1a1cef13b3c9
sodeeq olapade <olapadeabiodun21@email.com>
2019-08-06 14:52:58 +0100

    implement the user sign up route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,9 @@
-const httpStatus = require("http-status");
-const User = require("../models/user.model");
-const sendResponse = require("../helpers/response");
+const httpStatus = require('http-status');
+const User = require('../models/user.model');
+const sendResponse = require('../helpers/response');
+const { createUser } = require('../validations/user.validation');
+const gravatar = require('gravatar');
+const { Joi } = require('celebrate');
 
 /**
  * Load user and append to req.
@@ -8,18 +11,78 @@ const sendResponse = require("../helpers/response");
 exports.load = async (req, res, next, id) => {
   try {
     // Load user object from quarystring Id
-    return res.json(sendResponse(
-      httpStatus.NOT_FOUND,
-      "No such user exists!",
-      null,
-      null
-    ))
+    return res.json(
+      sendResponse(httpStatus.NOT_FOUND, 'No such user exists!', null, null)
+    );
   } catch (error) {
     next(error);
   }
-}; 
+};
 
 exports.getUsers = (req, res) => {
-  return res.json(sendResponse(200,'testing', null, null))
-}
+  return res.json(sendResponse(200, 'testing', null, null));
+};
 
+exports.signup = async (req, res) => {
+  const { error } = Joi.validate(req.body, createUser.body);
+  if (error)
+    return res.json(
+      sendResponse(
+        httpStatus.BAD_REQUEST,
+        'Bad Request',
+        null,
+        error.details[0].message
+      )
+    );
+
+  try {
+    const { name, email, password, isMentor, isAdmin } = req.body;
+
+    //check if user exists
+    let user = await User.getByEmail(email);
+    if (user) {
+      return res.json(
+        sendResponse(httpStatus.BAD_REQUEST, 'Bad Request', null, {
+          msg: 'Email already in use!'
+        })
+      );
+    }
+
+    //get user avatar
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
+
+    //create User instance
+    user = new User({
+      name,
+      email,
+      password,
+      avatar,
+      isMentor,
+      isAdmin
+    });
+
+    await user.save();
+    const payload = user.transform();
+    const token = await user.generateToken(payload);
+
+    console.log('token received:', token);
+
+    res.json(sendResponse(httpStatus.OK, 'Signup successful', token, null));
+  } catch (error) {
+    console.error('Error: ', error.message);
+    res
+      .status(500)
+      .json(
+        sendResponse(
+          httpStatus.INTERNAL_SERVER_ERROR,
+          'Server Error',
+          null,
+          error
+        )
+      );
+  }
+};
