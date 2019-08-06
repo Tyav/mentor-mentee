f96db19fb6e7a9a8ad879c33b5859a48991b6a94
iKnowJavaScript <remymartins0398@yahoo.com>
2019-08-06 13:06:45 +0100

    Create profilePicture mongoose schema

---
+++ models/profile.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const APIError = require('../helpers/APIError');
+const config = require('../config/env');
+
+/**
+ * UserPicture Schema
+ */
+const UserPictureSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  file: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  }
+});
+
+UserPictureSchema.statics = {
+  /**
+   *
+   * @param {String} id
+   * @returns {Promise<UserPictureSchema, APIError>}
+   */
+  async getByUserId(userId) {
+    let profileData = this.findOne({
+      userId
+    }).exec();
+    return profileData;
+  }
+};
+
+/**
+ * @typedef UserPictureSchema
+ */
+module.exports = mongoose.model('UserPicture', UserPictureSchema);
