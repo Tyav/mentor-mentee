09fe5c332b9ea9c9ea7159c8829aaa7b5411539c
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-06 12:33:17 +0100

    created a test file

---
+++ __test__/auth.test.js
@@ -0,0 +1,20 @@
+// let schedule = {
+//     day: 'Tuesday',
+//     time: {
+//         from: Date.now(),
+//         to: Date(),
+//     },
+//     slot: 5,
+//     isClosed: ()=> {return this.slot <= this.mentees.length},
+//     mentorId: 'sdsdfd',
+//     mentees: []
+
+// }
+
+// let request = {
+//     menteeId: 'sdss',
+//     scheduleId: 'sdfsds',
+//     requestMessage: 'please acept me',
+//     reponseMessage: '',
+//     status: 'pending' || 'approved' || 'rejected',
+// }
