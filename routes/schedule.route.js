491ef4a6301e6e286b43a2e293c30af15df81ce1
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-07 15:23:15 +0100

    refactored router in  schedule.route.js and pulled out used variables that were imported

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const httpStatus = require('http-status');
-const userRoutes = require('./user.route');
-const authRoutes = require('./auth.route');
-const sendResponse = require('../helpers/response');
-const scheduleModel = require('../models/schedule.model');
+// const httpStatus = require('http-status');
+// const userRoutes = require('./user.route');
+// const authRoutes = require('./auth.route');
+// const sendResponse = require('../helpers/response');
+// const scheduleModel = require('../models/schedule.model');
 const modelCtrl = require('../controllers/schedule.controller');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  return modelCtrl.createSchedule(req, res);
-});
+router.post('/', modelCtrl.createSchedule);
 
 module.exports = router;
