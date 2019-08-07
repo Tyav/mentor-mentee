c15f0428cf5b1ebaa2fa358103c0c2b3ac9ce3c3
MAC <mac@MACs-MacBook-Pro.local>
2019-08-07 11:41:59 +0100

    created mock for handling shcedule creation and completed slot request controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,3 +19,69 @@ exports.load = async (req, res, next, id) => {
 exports.getUsers = (req, res) => {
   return res.json(sendResponse(200, 'testing', null, null));
 };
+
+
+exports.createScheduleMock = async (req, res) => {
+  try {
+    const shcedule = new Schedule(req.body);
+    const result = await shcedule.save();
+    if (!result) {
+      return res.json(
+        sendResponse(
+          httpStatus.INTERNAL_SERVER_ERROR,
+          'An error occured submiting schedule'
+        )
+      );
+    }
+    return res.json(sendResponse(httpStatus.OK, 'Request submitted'));
+  } catch {
+    return res.json(sendResponse(httpStatus.NOT_FOUND, 'Something went wrong'));
+  }
+};
+
+exports.bookSlot = async (req, res) => {
+  try {
+    const schedule = await Schedule.findOne({
+      _id: req.params.scheduleID,
+      slots: { $gte: 0 }
+    });
+    if (!schedule) {
+      return res.json(sendResponse(httpStatus.NOT_FOUND, 'Schedule not found'));
+    }
+    if (schedule.slots < 1) {
+      return res.json(
+        sendResponse(
+          httpStatus.NOT_FOUND,
+          'There are no slots available for this schedule'
+        )
+      );
+    }
+    const requestMade = await Request.findOne({
+      scheduleId: schedule._id,
+      menteeId: req.body.menteeId
+    });
+    if (requestMade) {
+      return res.json(
+        sendResponse(httpStatus.NOT_FOUND, 'request already made')
+      );
+    }
+    const request = new Request({
+      scheduleId: req.params.scheduleID,
+      menteeId: req.body.menteeId
+    });
+    const requestResult = await request.save();
+    if (!requestResult) {
+      return res.json(
+        sendResponse(httpStatus.NOT_FOUND, 'the request was not submitted')
+      );
+    }
+    return res.json(sendResponse(httpStatus.OK, 'Request submitted'));
+  } catch {
+    return res.json(
+      sendResponse(
+        httpStatus.INTERNAL_SERVER_ERROR,
+        'something went wrong while submitting request'
+      )
+    );
+  }
+};
