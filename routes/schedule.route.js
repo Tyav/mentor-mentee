295e29e6e62d5d9831626ee0dd0bd4d8c6ada4aa
Tyav <tyav2greenz@gmail.com>
2019-08-13 15:04:45 +0100

    mount routes to get schedules

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const { celebrate: validate, errors } = require('celebrate');
 const scheduleCtrl = require('../controllers/schedule.controller');
+const requestCtrl = require('../controllers/request.controller');
 const paramValidation = require('../validations/schedule.validation');
-const decode = require('../middlewares/decode')
+const decode = require('../middlewares/decode');
 
 const router = express.Router();
 
-router.use(decode)
+router.use(decode);
 
-router.route('/')
-  .post(validate(paramValidation.create,{ abortEarly: false }), scheduleCtrl.createSchedule)
-/** GET api/v1/schedules  get all schedule */
-  .get(scheduleCtrl.getAllSchedules)
+router.param('scheduleId', scheduleCtrl.load);
+
+router
+  .route('/')
+  .post(validate(paramValidation.create, { abortEarly: false }), scheduleCtrl.createSchedule)
+  /** GET api/v1/schedules  get all schedule */
+  .get(scheduleCtrl.getAllSchedules);
+
+router
+  .route('/:scheduleId')
+  /** GET api/v1/schedules/id  get single schedule */
+  .get(scheduleCtrl.getSingleSchedule)
+  /** PUT api/v1/schedule/id edit schedule */
+  .put(scheduleCtrl.update);
+
+router
+  .route('/:scheduleId/requests')
+  /** GET api/v1/schedule/id/requests get all requests for a schedule */
+  .get(requestCtrl.getScheduleResquests);
 
 module.exports = router;
