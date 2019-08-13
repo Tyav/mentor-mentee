608f6140cb47571b85f206aeaa26029dfbdb4bd1
Tyav <tyav2greenz@gmail.com>
2019-08-13 14:45:23 +0100

    refactor code for decode middleware

--- middlewares/decode.js
+++ middlewares/decode.js
@@ -7,20 +7,8 @@ const tokendecoder = require('../helpers/TokenDecoder')
 
 module.exports = async (req, res, next) => {
   try {
-    // const authorization = req.headers['authorization'];
-    // if (!authorization) {
-    //   return res.json(
-    //     sendResponse(
-    //     httpStatus.UNAUTHORIZED,
-    //     'Invalid User',
-    //     null
-    //   ));
-    // }
-    // let token = authorization.split(' ')[1]
-    // return {token , decodeToken: jwt.decode(token, jwtSecret)}
     const { decodeToken } = tokendecoder(req, res)
     const {sub} = decodeToken
-    // let { sub } = jwt.decode(token, jwtSecret)
     const user = await User.get(sub);
     if (user) {
       req.sub = sub;
