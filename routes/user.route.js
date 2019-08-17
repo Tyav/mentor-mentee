9b70bee04f69172d65836e5e36b4c3b72edb9415
iKnowJavaScript <remymartins0398@yahoo.com>
2019-08-17 01:50:28 +0100

    Move update validation to main route

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,8 +4,11 @@ const paramValidation = require('../validations/user.validation');
 const userCtrl = require('../controllers/user.controller');
 const scheduleCtrl = require('../controllers/schedule.controller');
 const upload = require('../helpers/file-upload');
+const decode = require('../middlewares/decode');
 //const { profileImage } = require('../helpers/upload');
 const router = express.Router(); // eslint-disable-line new-cap
+
+router.use(decode);
 
 /** Load user when API with userId route parameter is hit */
 router.param('userId', userCtrl.load);
@@ -16,24 +19,22 @@ router
   /** GET /api/v1/users - get all users */
   .get(userCtrl.getUsers)
   /** POST /api/v1/user - creae a user */
-  .post(
-    validate(paramValidation.createUser, { abortEarly: false }),
-    userCtrl.signup,
-  );
+  .post(validate(paramValidation.createUser, { abortEarly: false }), userCtrl.signup);
+
+/** PUT /api/v1/user/userId update a user information */
+router.put(
+  '/me',
+  validate(paramValidation.updateUser, { abortEarly: false }),
+  userCtrl.updateProfile
+);
 
 /** PUT /api/v1/users/:userId/avatar */
-router.put(
-  '/:userId/images',
-  upload('avatar').single('avatar'),
-  userCtrl.updateAvatar,
-);
+router.put('/:userId/images', upload('avatar').single('avatar'), userCtrl.updateAvatar);
 
 router
   .route('/:userId')
   /** GET /api/v1/user/userId gets a user by id */
-  .get(userCtrl.getUser)
-  /** PUT /api/v1/user/userId update a user information */
-  .put(userCtrl.updateProfile);
+  .get(userCtrl.getUser);
 
 router.get('/:userId/schedules', scheduleCtrl.getUserSchedules);
 
