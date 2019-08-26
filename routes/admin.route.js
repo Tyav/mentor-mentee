4a2f421af5181451638e208821a3e3d63e851d20
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-26 15:33:24 +0100

    added login route for admin

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,25 +1,36 @@
 const express = require('express');
 const { celebrate: validate, errors } = require('celebrate');
 const paramValidation = require('../validations/user.validation');
+const authValidation = require('../validations/auth.validation');
 const decode = require('../middlewares/decode');
 const adminAuth = require('../middlewares/adminAuth.middleware');
+const adminCtrl = require('../controllers/admin.controller');
 const userCtrl = require('../controllers/user.controller');
 const router = express.Router(); // eslint-disable-line new-cap
+
+/** Admin Login /api/v1/login */
+router.post(
+  '/login',
+  validate(authValidation.login, { abortEarly: false }),
+  adminCtrl.login
+);
+
 router.use(decode);
+
 /** Load user when API with userId route parameter is hit */
 router.param('userId', userCtrl.load);
 
+/** Ensures the user is an admin */
 router.use(adminAuth);
 
 router
   .route('/')
-
   /** GET /api/v1/users - get all users */
   .get(userCtrl.getUsers)
-  /** POST /api/v1/user - creae a user */
+  /** POST /api/v1/admin - creae a user */
   .post(
     validate(paramValidation.createUser, { abortEarly: false }),
-    userCtrl.signup
+    adminCtrl.signup
   );
 
 module.exports = router;
