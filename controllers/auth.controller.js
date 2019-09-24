7a43ce3aafe6ff91d499ed766e727f12fa6a31c9
Tyav <tyav2greenz@gmail.com>
2019-09-24 03:01:14 +0100

    rename tokenEncoder

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,7 +6,7 @@ const sendResponse = require('../helpers/response');
 const ForgotPassword = require('../models/passwordReset.model');
 const sendMail = require('../helpers/SendMail');
 const messages = require('../helpers/mailMessage');
-const tokenDecoder = require('../helpers/tokenDecoder');
+const tokenDecoder = require('../helpers/tokensDecoder');
 const { forgotPassword } = require('../helpers/mailMessage');
 
 exports.forgotPassword = async (req, res) => {
