76006e7fa9396fb8e79171fc4c971f0893ecb371
whitehox <oketegah@gmail.com>
2019-10-22 10:08:17 +0100

    added title filed to idp create and update validation

--- validations/idp.validation.js
+++ validations/idp.validation.js
@@ -1,36 +1,39 @@
-const { Joi,  } = require('celebrate');
+const { Joi } = require('celebrate');
 
 module.exports = {
   createIdp: {
     body: {
-      mentor: Joi.string().regex(/^[a-fA-F0-9]{24}$/).required(),
+      mentor: Joi.string()
+        .regex(/^[a-fA-F0-9]{24}$/)
+        .required(),
+      title: Joi.string().required(),
       goal: Joi.string().max(500),
       plan: Joi.string().max(500),
       outcome: Joi.string().max(500),
       result: Joi.string().max(500),
-      comment: Joi.string().max(500)
-    }
+      comment: Joi.string().max(500),
+    },
   },
   updateIdp: {
     body: {
+      title: Joi.string().max(500),
       goal: Joi.string().max(500),
       plan: Joi.string().max(500),
       outcome: Joi.string().max(500),
       result: Joi.string().max(500),
-      comment: Joi.string().max(500)
+      comment: Joi.string().max(500),
     },
     params: {
       id: Joi.string()
         .regex(/^[a-fA-F0-9]{24}$/)
-        .required()
-    }
-
+        .required(),
+    },
   },
-  deleteIdp : {
+  deleteIdp: {
     params: {
       id: Joi.string()
         .regex(/^[a-fA-F0-9]{24}$/)
-        .required()
-    }
-  }
-}
+        .required(),
+    },
+  },
+};
