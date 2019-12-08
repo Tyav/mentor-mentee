8ebb99fce631265496d7febcd1b39776a41e536e
whitehox <oketegah@gmail.com>
2019-12-08 23:54:26 +0100

    added idp tp request model

--- models/request.model.js
+++ models/request.model.js
@@ -2,36 +2,44 @@ const mongoose = require('mongoose');
 const APIError = require('../helpers/APIError');
 const httpStatus = require('http-status');
 
-const RequestSchema = new mongoose.Schema({
-  mentee: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'User'
+const RequestSchema = new mongoose.Schema(
+  {
+    mentee: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    schedule: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Schedule',
+    },
+    idp: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Idp',
+    },
+    message: {
+      type: String,
+      maxlength: 250,
+    },
+    response: {
+      type: String,
+      maxlength: 250,
+    },
+    status: {
+      type: String,
+      enum: ['Pending', 'Approved', 'Rejected', 'Cancelled'],
+      default: 'Pending',
+      required: true,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  schedule: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'Schedule'
-  },
-  message: {
-    type: String,
-    maxlength: 250
-  },
-  response: {
-    type: String,
-    maxlength: 250
-  },
-  status: {
-    type: String,
-    enum: ['Pending', 'Approved', 'Rejected', 'Cancelled'],
-    default: 'Pending',
-    required: true
-  },
-  deleted: {
-    type: Boolean,
-    default: false
-  }
-},{timestamps: true});
+  { timestamps: true },
+);
 
 RequestSchema.methods = {};
 
@@ -45,31 +53,34 @@ RequestSchema.statics = {
     } catch (error) {
       throw new APIError({
         message: error.message,
-        status: httpStatus.BAD_REQUEST
+        status: httpStatus.BAD_REQUEST,
       });
     }
   },
   async getBy(option) {
     try {
       return await this.find(option)
-        .populate({ path: 'mentee', select: '_id name avatar skills isMentor email' })
+        .populate({
+          path: 'mentee',
+          select: '_id name avatar skills isMentor email',
+        })
         .populate({
           path: 'schedule',
           select: '_id day time mentor',
           populate: {
             path: 'mentor',
-            select: '_id name avatar skills'
-          }
+            select: '_id name avatar skills',
+          },
         })
         .sort('field -createAt')
         .exec();
     } catch (error) {
       throw new APIError({
         message: error.message,
-        status: httpStatus.BAD_REQUEST
+        status: httpStatus.BAD_REQUEST,
       });
     }
-  }
+  },
 };
 
 module.exports = mongoose.model('Request', RequestSchema);
